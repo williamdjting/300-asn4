c105c929b4ad3c2df180126a34a0d12d2979413b
William Ting <williamdjting@gmail.com>
2023-07-24 20:36:22 -0700

    new files

---
+++ makefile
@@ -0,0 +1,7 @@
+all: myls
+
+main: myls.c
+	gcc -g -Wall -o myls myls.c
+
+clean:
+	$(RM) myls
---
+++ myls.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Function prototypes (if needed)
+// e.g., void myFunction();
+
+int main() {
+    // Your code here
+
+    // Example: Printing "Hello, World!"
+    printf("Hello, World!\n");
+
+    // Function calls (if needed)
+    // e.g., myFunction();
+
+    return 0;
+}
