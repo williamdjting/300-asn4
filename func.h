02d32328f6f36c432600d63e3d70ca6ae469b9f2
Mitchell Liu <32404146+Mitchell-Liu@users.noreply.github.com>
2023-07-29 00:07:33 -0700

    Add files via upload

--- func.h
+++ func.h
@@ -8,34 +8,47 @@
 #define MAX 256
 
 struct fileArgs{
-    char options[3];
-    char file_list[MAX][MAX];
+    char options[3];    // Options for ls command, e.g., 'i' for inode number display
+    char file_list[MAX][MAX];   // Array to store a list of files (currently not used)
 };
+
+// Function to list files in the specified directory with provided options
 void ls(struct fileArgs myFile, const char *dir, int op_a, int op_l){
-    struct  dirent *d;
+    struct dirent *d;
     DIR *dh = opendir(dir);
+
+    // Check if the directory can be opened
     if (!dh){
-        if (errno=ENOENT){
-            perror("Directory does not exit");
+        if (errno == ENOENT){
+            perror("Directory does not exist");
         }
         else{
             perror("Unable to read directory");
         }
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);   // Exit the program with failure status
     }
-    while((d = readdir(dh))!=NULL){
-        if(!op_a && d->d_name[0]=='.'){
+
+    // Loop through directory entries and list files
+    while ((d = readdir(dh)) != NULL){
+        // Skip hidden files if op_a is not set (0)
+        if (!op_a && d->d_name[0] == '.'){
             continue;
         }
-        for(int i=0;myFile.options[i]!=NULL;i++){
-            if(myFile.options[i]=='i'){
-                printf("%ju         ",d->d_ino);
+
+        // Iterate through the options provided in myFile struct
+        for (int i = 0; myFile.options[i] != '\0'; i++){
+            if (myFile.options[i] == 'i'){
+                printf("%ju         ", d->d_ino);  // Display inode number
             }
         }
-        printf("%s ", d->d_name);
-        if(op_l){
-            printf("\n");
+        
+        printf("%s ", d->d_name);   // Print the name of the file/directory
+        
+        if (op_l){
+            printf("\n");   // Print a newline if long format option (op_l) is set
         }
     }
-    
+
+    closedir(dh);   // Close the directory after listing its contents
 }
+
--- myls.c
+++ myls.c
@@ -9,37 +9,50 @@
 
 int main( int argc, char *argv[] )  {
    struct fileArgs myFile;
-   
+
    int option;
-   int num=0;
-   while((option=getopt(argc,argv,"ilR"))!=-1){
-      switch (option)
-      {
-      case 'i':
-         myFile.options[num] = 'i';
-         num++;
-         printf("i\n");
-         break;
-      case 'l':
-         myFile.options[num] = 'l';
-         num++;
-         printf("l\n");
-         break;
-      case 'R':
-         myFile.options[num] = 'R';
-         num++;
-         printf("R\n");
-         break;
-      default:
-         break;
+   int num = 0;
+
+   // Process command-line options using getopt
+   while ((option = getopt(argc, argv, "ilR")) != -1) {
+      switch (option) {
+         case 'i':
+            // Option 'i' is present, add it to the options array in myFile
+            myFile.options[num] = 'i';
+            num++;
+            printf("i\n"); // Debugging: Print to confirm the 'i' option is found
+            break;
+         case 'l':
+            // Option 'l' is present, add it to the options array in myFile
+            myFile.options[num] = 'l';
+            num++;
+            printf("l\n"); // Debugging: Print to confirm the 'l' option is found
+            break;
+         case 'R':
+            // Option 'R' is present, add it to the options array in myFile
+            myFile.options[num] = 'R';
+            num++;
+            printf("R\n"); // Debugging: Print to confirm the 'R' option is found
+            break;
+         default:
+            // Invalid option or missing argument, do nothing for now
+            break;
       }
    }
-   for(int i = 0; i<argc-num;i++){
-     strcpy(myFile.file_list[i],argv[num+i]);
+
+   // Store the remaining command-line arguments as file_list in myFile struct
+   for (int i = 0; i < argc - num; i++) {
+      strcpy(myFile.file_list[i], argv[num + i]);
    }
-   for(int i=0;i<3;i++){
-      printf("testing option: %c\n",myFile.options[i]);
-      printf("testing direciory %s\n",myFile.file_list[i]);
+
+   // Debugging: Print the options and file_list
+   for (int i = 0; i < 3; i++) {
+      printf("testing option: %c\n", myFile.options[i]);
+      printf("testing directory: %s\n", myFile.file_list[i]);
    }
-   ls(myFile,".",0,1);
+
+   // Call the ls function with the provided options and file_list
+   ls(myFile, ".", 0, 1); // Listing current directory with options: 'i', long format
+
+   return 0;
 }
